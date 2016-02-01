677768e1b072c55f84de67ad19b92d8931a7b272
Dharti Tarapara <dtara003@hammer.cs.ucr.edu>
2016-02-01 14:13:25 -0800

    Functional Extra Credit

---
+++ main.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <vector>
+#include <sstream>
+#include <unistd.h>
+
+// unistd.h used later for execution of commands
+
+using namespace std;
+
+int main() {
+    string user = getlogin();
+    char hostname[100];
+    gethostname(hostname, 100);
+    
+    cout << "[" << user << "@" << hostname << "]$ ";
+    
+    /*while(1) {
+        string input;
+        getline(cin, input);
+        
+        stringstream str(input);
+        string s;
+        
+        vector <string> words;
+
+        while (str >> s) {
+            words.push_back(s);
+
+            if (s == "exit") {
+                return 0;
+            } else {
+                cout << s << endl;
+            }
+        }
+
+        // check
+        // cout << input << endl;
+    }*/
+
+    return 0;
+}

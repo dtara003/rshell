23c94fd2ab323023412b0d076ce01c868e82737c
Dharti Tarapara <dtara003@hammer.cs.ucr.edu>
2016-02-10 19:04:53 -0800

    attempted to fix Parse.h spacing

--- src/Connector.h
+++ src/Connector.h
@@ -2,18 +2,70 @@
 #define CONNECTOR_H
 
 #include "Shell.h"
+#include <iostream>
 
 using namespace std;
 
 //Composite class for Connectors(&& and ||)
 class Connector : public Shell {
-    protected:
+    public:
         Shell* left;
         Shell* right;
 
-    public:
+        // constructor
         Connector() {};
-        bool execute() {return true;}
+
+        bool execute() {
+            return true;
+        };
+};
+
+class Semi : public Connector {
+    public:
+        // constructor
+        Semi() {};
+        Semi(Shell* l, Shell* r) : left(l), right(r) {};
+
+        bool execute() {
+            //
+        };
+};
+
+class And : public Connector {
+	public:
+		And() {};
+		
+		//a constructor that can have a null left pointer
+		And(Shell* l, Shell* r) : left(l), right(r) {};
+
+		//set the left pointer to whatever the argument is pointing at
+		//void setLeft(Shell* s) {
+			//left = s;
+		//};
+
+		//execute function that will perform as &&
+		bool execute() {
+			if (left->execute() == true) {
+				right->execute();
+				return true;
+			}
+			else {
+				return false;
+            }
+		};
+};
+
+class Or : public Connector {
+	public:
+        // constructors
+        Or() {};
+		Or(Shell* l, Shell* r) : left(l), right(r) {};
+
+		void execute() {
+			//assuming execute returns the command name for testing
+			cout << "Executed: " << left->execute() << " or " << "Executed: "
+                 << right->execute();
+		};
 };
 
 #endif
--- src/Prompt.h
+++ src/Prompt.h
@@ -16,17 +16,16 @@ class Prompt {
         Shell* ptr;
         //vector <Shell*> roots;
         vector <int> connVals;
-        
-        
+      
     public:
-		Prompt() {
+        Prompt() {
             ptr = NULL;
         };
 
-		Prompt(string s, vector <int> v) : input(s), connVals(v) {};
+        Prompt(string s, vector <int> v) : input(s), connVals(v) {};
 		
-		//Shell(string )
-		
+        //Shell(string ) {}
+
         void promptUser() {
             string user = getlogin();
             char hostname[100];
@@ -51,194 +50,194 @@ class Prompt {
         };
         
         // remove comments
-		void rmComments(string &input) {
-		    for (unsigned int i = 0; i < input.size(); ++i) {
+        void rmComments(string &input) {
+            for (unsigned int i = 0; i < input.size(); ++i) {
                 if (input.at(i) == '#') {
                     input = input.substr(0, i);
                     i = input.size();
                 }
             }
-		};
+        };
 		
-		void parse() {
-		    // lines 70 - 83 probably not a viable method of interpreting
-		    // command line inputs
-                // weak method error-catching
-                // just gets rid of starting and ending errors altogether
+        void parse() {
+        // lines 70 - 83 probably not a viable method of interpreting
+        // command line inputs
+        // weak method error-catching
+        // just gets rid of starting and ending errors altogether
                 
-		    // remove extra spaces from start and end of input string
-		    trim(input);
+        // remove extra spaces from start and end of input string
+        trim(input);
 		    
-		    // remove any leading or trailing connectors
-		    while ( (input.at(input.size() - 1) == '&') || (input.at(input.size()
-		    - 1) == '|') || (input.at(input.size() - 1) == ';') ) {
-		        input = input.substr(0, input.size() - 2);
-                
-		        trim(input);
-		    }
+        // remove any leading or trailing connectors
+        while ( (input.at(input.size() - 1) == '&') || (input.at(input.size()
+        - 1) == '|') || (input.at(input.size() - 1) == ';') ) {
+            input = input.substr(0, input.size() - 2);
+               
+            trim(input);
+        }
 		    
-		    while ( (input.at(0) == '&') || (input.at(0) == '|') || (input.at(0)
-		    == ';') ) {
-		        input = input.substr(1, input.size() - 1);
-
-                trim(input);
-		    }
-		    
-		    //trim(input);
-		    
-		    // check
-		    cout << input << endl << "trimmed" << endl;
-		   		    
-		    // 1 if semicolon, 2 if and, 3 if or
-		    unsigned int i = 0;
-		    bool quotes = false;
-		    while (i < input.size()) {
-		        // toggles to check for quotes used for commands like echo
-		        if (input.at(i) =='\"') {
-		            if (!quotes) {
-		                quotes = true;
-		            } else {
-		                quotes = false;
-		            }
-		        }
-		        
-		        if (!quotes) {
-    		        if (input.at(i) == ';') {
-    		            // connVals.push_back(1);
-    		            
-    		            if (ptr == NULL) {
-    		            	// create Command object of everything up until that
-    		            	// semicolon
-    		            	ptr = new Command(trim(input.substr(0, i)));
-    		            	input = input.substr(i + 1, input.size() - 1);
-    		            	i = 0;
-                            connVals.push_back(1);
-    		            } else {
-    		                if (connVals.at(connVals.size() - 1) == 1) {
-                                // need new pointer variable to hold new connector
-                                // object
-                                Shell* a = new Command(trim(input.substr(0, i)));
-                                input = input.substr(i + 1, input.size() - 1);
-                                Shell* b = new Semi(ptr, a);
-                                
-                                // Shell* pointer now points to semicolon
-                                ptr = b;
-                                i = 0;
-                                connVals.push_back(1);
-                            } else if (connVals.at(connVals.size() - 1) == 2) {
-                                Shell* a = new Command(trim(input.substr(0, i)));
-                                input = input.substr(i + 1, input.size() - 1);
-                                Shell* b = new And(ptr, a);
-
-                                ptr = b;
-                                i = 0;
-                                connVals.push_back(1);
-                            } else if (connVals.at(connVals.size() - 1) == 3) {
-                                Shell* a = new Command(trim(input.substr(0, i)));
-                                input = input.substr(i + 1, input.size() - 1);
-                                Shell* b = new Or(ptr, a);
-
-                                ptr = b;
-                                i = 0;
-                                connVals.push_back(1);
-                            }
-                        }
-                    } else if (input.at(i) == '&' && input.at(i + 1) == '&') {
-    		            // connVals.push_back(2);
-    		            
-    		            if (ptr == NULL) {
-    		            	// create Command object of everything up until the
-    		            	// and symbols
-    		            	ptr = new Command(trim(input.substr(0, i)));
-    		            	input = input.substr(i + 2, input.size() - 1);
-    		            	i = 0;
-                            connVals.push_back(2);
-    		            } else {
-    		            	if (connVals.at(connVals.size() - 1) == 1) {
-                                Shell* a = new Command(trim(input.substr(0, i)));
-                                input = input.substr(i + 2, input.size() - 1);
-                                Shell* b = new Semi(ptr, a);
-
-                                ptr = b;
-                                i = 0;
-                                connVals.push_back(3);
-                            } else if (connVals.at(connVals.size() - 1) == 2) {
-                                Shell* a = new Command(trim(input.substr(0, i)));
-                                input = input.substr(i + 2, input.size() - 1);
-                                Shell* b = new And(ptr, a);
-
-                                ptr = b;
-                                i = 0;
-                                connVals.push_back(2);
-                            } else if (connVals.at(connVals.size() - 1) == 3) {
-                                Shell* a = new Command(trim(input.substr(0, i)));
-                                input = input.substr(i + 2, input.size() - 1);
-                                Shell* b = new Or(ptr, a);
-
-                                ptr = b;
-                                i = 0;
-                                connVals.push_back(2);
-                            }
-                        }
-    		        } else if (input.at(i) == '|' && input.at(i + 1) == '|') {
-    		            // connVals.push_back(3);
-    		            
-    		            if (ptr == NULL) {
-    		            	// create Command object of everything up until the
-    		            	// or symbols
-    		            	ptr = new Command(trim(input.substr(0, i)));
-    		            	
-    		            	input = input.substr(i + 2, input.size() - 1);
-    		            	i = 0;
-                            connVals.push_back(3);
-    		            } else {
-    		            	if (connVals.at(connVals.size() - 1) == 1) {
-                                Shell* a = new Command(trim(input.substr(0, i)));
-                                input = input.substr(i + 2, input.size() - 1);
-                                Shell* b = new Semi(ptr, a);
-
-                                ptr = b;
-                                i = 0;
-                                connVals.push_back(3);
-                            } else if (connVals.at(connVals.size() - 1) == 2) {
-                                Shell* a = new Command(trim(input.substr(0, i)));
-                                input = input.substr(i + 2, input.size() - 1);
-                                Shell* b = new And(ptr, a);
-
-                                ptr = b;
-                                i = 0;
-                                connVals.push_back(3);
-                            } else if (connVals.at(connVals.size() - 1) == 3) {
-                                Shell* a = new Command(trim(input.substr(0, i)));
-                                input = input.substr(i + 2, input.size() - 1);
-                                Shell* b = new Or(ptr, a);
-
-                                ptr = b;
-                                i = 0;
-                                connVals.push_back(3);
-                            }
-    		            }
-    		        }
-		        }
-		        
-		        i++;
-		    }
-
-            // account for final command
+        while ( (input.at(0) == '&') || (input.at(0) == '|') || (input.at(0)
+        == ';') ) {
+            input = input.substr(1, input.size() - 1);
 
             trim(input);
+        }
 		    
-		    for (unsigned int i = 0; i < connVals.size(); ++i) {
-		    	cout << connVals.at(i) << " ";
-		    }
+        //trim(input);
 		    
-		    cout << endl << input << endl;
-		};
+        // check
+        // cout << input << endl << "trimmed" << endl;
+		   		    
+        // 1 if semicolon, 2 if and, 3 if or
+        unsigned int i = 0;
+        bool quotes = false;
+        while (i < input.size()) {
+            // toggles to check for quotes used for commands like echo
+            if (input.at(i) =='\"') {
+                if (!quotes) {
+                    quotes = true;
+                } else {
+                    quotes = false;
+                }
+            }
+		        
+            if (!quotes) {
+                if (input.at(i) == ';') {
+                    // connVals.push_back(1);
+    		            
+                    if (ptr == NULL) {
+                        // create Command object of everything up until that
+                        // semicolon
+                        ptr = new Command(trim(input.substr(0, i)));
+                        input = input.substr(i + 1, input.size() - 1);
+                        i = 0;
+                        connVals.push_back(1);
+                    } else {
+                        if (connVals.at(connVals.size() - 1) == 1) {
+                            // need new pointer variable to hold new connector
+                            // object
+                            Shell* a = new Command(trim(input.substr(0, i)));
+                            input = input.substr(i + 1, input.size() - 1);
+                            Shell* b = new Semi(ptr, a);
+                                
+                            // Shell* pointer now points to semicolon
+                            ptr = b;
+                            i = 0;
+                            connVals.push_back(1);
+                        } else if (connVals.at(connVals.size() - 1) == 2) {
+                            Shell* a = new Command(trim(input.substr(0, i)));
+                            input = input.substr(i + 1, input.size() - 1);
+                            Shell* b = new And(ptr, a);
+
+                            ptr = b;
+                            i = 0;
+                            connVals.push_back(1);
+                        } else if (connVals.at(connVals.size() - 1) == 3) {
+                            Shell* a = new Command(trim(input.substr(0, i)));
+                            input = input.substr(i + 1, input.size() - 1);
+                            Shell* b = new Or(ptr, a);
+
+                            ptr = b;
+                            i = 0;
+                            connVals.push_back(1);
+                        }
+                    }
+                } else if (input.at(i) == '&' && input.at(i + 1) == '&') {
+                    // connVals.push_back(2);
+    		            
+                    if (ptr == NULL) {
+                        // create Command object of everything up until the
+                        // and symbols
+                        ptr = new Command(trim(input.substr(0, i)));
+                        input = input.substr(i + 2, input.size() - 1);
+                        i = 0;
+                        connVals.push_back(2);
+                    } else {
+                        if (connVals.at(connVals.size() - 1) == 1) {
+                            Shell* a = new Command(trim(input.substr(0, i)));
+                            input = input.substr(i + 2, input.size() - 1);
+                            Shell* b = new Semi(ptr, a);
+
+                            ptr = b;
+                            i = 0;
+                            connVals.push_back(3);
+                        } else if (connVals.at(connVals.size() - 1) == 2) {
+                            Shell* a = new Command(trim(input.substr(0, i)));
+                            input = input.substr(i + 2, input.size() - 1);
+                            Shell* b = new And(ptr, a);
+
+                            ptr = b;
+                            i = 0;
+                            connVals.push_back(2);
+                        } else if (connVals.at(connVals.size() - 1) == 3) {
+                            Shell* a = new Command(trim(input.substr(0, i)));
+                            input = input.substr(i + 2, input.size() - 1);
+                            Shell* b = new Or(ptr, a);
+
+                            ptr = b;
+                            i = 0;
+                            connVals.push_back(2);
+                        }
+                    }
+                } else if (input.at(i) == '|' && input.at(i + 1) == '|') {
+                    // connVals.push_back(3);
+    		            
+                    if (ptr == NULL) {
+                        // create Command object of everything up until the
+                        // or symbolptr = new Command(trim(input.substr(0, i)));
+                        ptr = new Command(trim(input.substr(0, i)));
+                        input = input.substr(i + 2, input.size() - 1);
+                        i = 0;
+                        connVals.push_back(3);
+                    } else {
+                        if (connVals.at(connVals.size() - 1) == 1) {
+                            Shell* a = new Command(trim(input.substr(0, i)));
+                            input = input.substr(i + 2, input.size() - 1);
+                            Shell* b = new Semi(ptr, a);
+
+                            ptr = b;
+                            i = 0;
+                            connVals.push_back(3);
+                        } else if (connVals.at(connVals.size() - 1) == 2) {
+                            Shell* a = new Command(trim(input.substr(0, i)));
+                            input = input.substr(i + 2, input.size() - 1);
+                            Shell* b = new And(ptr, a);
+
+                            ptr = b;
+                            i = 0;
+                            connVals.push_back(3);
+                        } else if (connVals.at(connVals.size() - 1) == 3) {
+                            Shell* a = new Command(trim(input.substr(0, i)));
+                            input = input.substr(i + 2, input.size() - 1);
+                            Shell* b = new Or(ptr, a);
+
+                            ptr = b;
+                            i = 0;
+                            connVals.push_back(3);
+                        }
+                    }
+                }
+            }
+		        
+            i++;
+        }
+
+        // account for final command
+        trim(input);
+            
+
+
+        for (unsigned int i = 0; i < connVals.size(); ++i) {
+            cout << connVals.at(i) << " ";
+        }
+		    
+        cout << endl << input << endl;
+    };
 		
-		void freeMem() {
-		    // DO NOT FORGET
-		    // FREE ANY MEMORY THAT WAS ALLOCATED
-		};
+    void freeMem() {
+        // DO NOT FORGET
+        // FREE ANY MEMORY THAT WAS ALLOCATED
+    };
     
 };
 

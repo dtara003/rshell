18458be45e2bcd1392b82fb031d2a0d5be0d3804
Gian Ocampo <gocam001@hammer.cs.ucr.edu>
2016-02-12 21:04:55 -0800

    Test script

--- tests/multi_command.sh
+++ tests/multi_command.sh
@@ -1,1 +1,10 @@
+#!/bin/sh
 
+#go to the directory of where a.out is(rshell in this case)
+cd ..
+
+echo hello || ls && date < ./a.out
+ls && xyz && echo hey && date < ./a.out
+ls -a ||  sup && git branch < ./a.out
+cd Dir && mkdir Dir < ./a.out
+mkdir Dir && rm -r Dir || echo hey < ./a.out
--- tests/single_command.sh
+++ tests/single_command.sh
@@ -1,1 +1,19 @@
+#!/bin/sh
 
+#go to rshell
+cd ..
+#make(if you didnt make already)
+make
+#go to src
+cd src
+
+#echo will return ls which will go into ./a.out's input
+echo "ls" | ./a.out
+
+#if you want to echo the echo command echo twice
+#for example
+echo "echo hello" | ./a.out
+
+#make -C ..
+#./../a.out << 'EOF'
+#ls

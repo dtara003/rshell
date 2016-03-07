38b175c275966e19d2b0b9151e49d836f235b9e3
Gian Ocampo <gocam001@hammer.cs.ucr.edu>
2016-03-06 21:14:22 -0800

    added two of the tests scripts

--- tests/commented_command.sh
+++ tests/commented_command.sh
@@ -18,3 +18,30 @@ g++ #test.cpp########
 #
 echo hello || #exit;
 ##|| ls -a
+test #-d /home/csmajs/gocam001/CS100/rshell
+[ #-e /home/csmajs/gocam001/CS100/rshell ]
+[ -e # /home/csmajs/gocam001/CS100/rshell ] && echo A;
+# [ /home/csmajs ]
+#test -f /home/csmajs/gocam001/CS100/rshell && ls
+test -f #/home/csmajs/gocam001/CS100/rshell/src/Connector.h
+test /home/csmajs/gocam001/CS100 && #ls -a
+[ -e /home/csmajs/gocam001/CS100 ] || #date
+[ -#f /home/csmajs/gocam001/CS100/rshell/src/Shell.h ] 
+[ ################## ]
+test ######## /home/csmajs/gocam001/CS100
+test -f /home/csmajs/gocam001/CS100/rshell/src/Connector.h #
+[ ] #
+[ # ]
+test ##### /home/csmajs/gocam001/CS100 #
+[] #[]
+test /#home/csmajs/gocam001/CS100
+[ -d /#home/csmajs/gocam001/CS100/rshell
+####################### test
+&#####test asfsafkls
+echo A # (echo B && echo C)
+(#date && ls -a) && mkdir Dir
+touch test.cpp (&& #(asfsafs && rm test.cpp))
+(echo A && echo B) || (echo C && echo D) ##
+#()
+(ls -j || ls -a) && (test -e /home/csmajs/gocam001/CS100 && date #)
+
---
+++ tests/test_test.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+[]
+[ ]
+[ -d
+[ -d /home/csmajs/gocam001/CS100
+[ -e
+[-f
+[ -f
+[ -d /home/csmajs/gocam001/CS100 ]
+[ -f /home/csmajs/gocam001/CS100 ]
+[ -f /home/csmajs/gocam001/CS100/rshell/src/Command.h ]
+[ -e /home/csmajs/gocam001/CS100 ]
+[-e]
+[-d /home/csmajs/gocam001/CS100]
+test [ -d /csmajs/gocam001/CS100 ]
+test
+test -e /home/csmajs/gocam001/CS100
+test -f /home/csmajs/gocam001/CS100/rshell/src/Shell.h
+[ test -d /home/csmajs/gocam001/CS100/rshell ]
+test /home/csmajs/gocam001/CS100/rshell/src/Shell.h
+test /home/csmajs/gocam001/CS100
+test /sfsaf/sfa
+test/home/csmajs/gocam001
+test/sdfsaf
+test /sdfsafs a
+test /home/csmajs/gocam001/rshell/src/Shell.h s
